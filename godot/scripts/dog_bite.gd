1427e3b1711557370b8adafa93c55d81ef4ea7ee
Aidan Nuzum-Clark <aidan@Aidans-Laptop.local>
2024-11-09 16:21:48 -0500

    add: slug movement and collisions with player and bite

--- godot/scripts/dog_bite.gd
+++ godot/scripts/dog_bite.gd
@@ -6,8 +6,16 @@ func _process(delta):
 	
 	if Input.is_action_just_pressed("attack"):
 		$AnimatedSprite2D.play()
+		changeBite(false)
+		
 	
 	
+func changeBite(status: bool):
+	$BigBite.disabled = status
+	$SmallBite.disabled = status
 	
 	
-	
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	changeBite(true)
